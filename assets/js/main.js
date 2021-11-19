bdf5b40137a691253b3707434fe0b733f069e874
exe <inostroza28.c@gmail.com>
2021-11-19 01:42:49 -0300

    Final entrega

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+$(function(){
+    
+    
+    
+    $("#tg1").click(function(){
+    $( "#pone" ).css({
+        color: "cyan",
+    });
+  });
+
+
+  $("#tg2").click(function(){
+    $( "#ptwo" ).css({
+        color: "cyan",
+    });
+  });
+
+  $("#tg3").click(function(){
+    $( "#ptree" ).css({
+        color: "cyan",
+    });
+  });
+
+  $("#des").click(function(){
+    $( "p" ).toggle(800);
+  });
+
+
+
+
+
+
+});
